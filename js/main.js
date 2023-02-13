af4a5e29d90c4a135464927109a1d14aa75ca08c
Istam Yuldoshov <istam@MBP-Istam.netis.cc>
2023-02-13 22:55:21 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let users = [
+  { id: 1, name: "Abror", age: 16 },
+  { id: 2, name: "Abdulloh", age: 19 },
+  { id: 3, name: "Abdussamad", age: 17 },
+  { id: 4, name: "Abdulaziz", age: 26 },
+  { id: 5, name: "Abdulatif", age: 25 },
+  { id: 6, name: "Abdulboqiy", age: 14 },
+];
+
+// console.log(users);
+
+users.forEach(function (arg1, arg2, arg3) {
+    console.log(arg1, arg2, arg3);
+});
+ 
